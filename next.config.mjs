0f312be9aa9772ad2df28e3e05fd430515629e9f
Jai Pandey <pandeyjai041003@gmail.com>
2024-05-13 11:49:27 +0530

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [{ protocol: "https", hostname: "www.dominos.co.in" }, {protocol: "https", hostname: "images.unsplash"}],
+    remotePatterns: [{ protocol: "https", hostname: "www.dominos.co.in" }, {protocol: "https", hostname: "images.unsplash.com"}],
   },
 };
 
